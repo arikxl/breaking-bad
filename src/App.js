5e2eec712a965714531512487fe4ed1abb22619d
Arik Alexandrov <arikxl@gmail.com>
2021-09-25 03:20:41 +0300

    extra randoms

--- src/App.js
+++ src/App.js
@@ -5,27 +5,38 @@ import './App.css';
 import AppHeader from './components/AppHeader';
 import CharactersList from './components/CharactersList';
 import Search from './components/Search';
+import RandomDeath from './components/RandomDeath';
+import RandomQuote from './components/RandomQuote';
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
+  const [randomDeath, setRandomDeath] = useState({});
+  const [randomQuote, setRandomQuote] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [query, setQuery] = useState('');
-
+  
   useEffect(() => { 
     const fetchItems = async () => { 
       const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
-      const randomDeath = await axios(`https://www.breakingbadapi.com/api/random-death`);
-      console.log( randomDeath.data)
-
+      const randomDeathResult = await axios(`https://www.breakingbadapi.com/api/random-death`);
+      const randomDeathQuote = await axios(`https://www.breakingbadapi.com/api/quote/random`);
       setCharacters( result.data);
+      setRandomQuote( randomDeathQuote.data)
+      setRandomDeath( randomDeathResult.data)
       setIsLoading(false);
     }
     fetchItems()
   },[query])
-
+  
   return (
     <div className="App">
       <AppHeader />
+      <div className="btns-container">
+        <button className= "random-btn btn" >Random Death</button>
+        <button className= "quote-btn btn">Random Quote</button>
+      </div>
+      <RandomQuote randomQuote={randomQuote[0]} isLoading={isLoading}/>
+      <RandomDeath randomDeath={randomDeath} isLoading={isLoading}/>
       <Search getQuery={(inputWord) => setQuery(inputWord)} />
       <CharactersList isLoading={isLoading} characters={characters}/>
     </div>
--- src/components/RandomDeath.jsx
+++ src/components/RandomDeath.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Loader from './Loader';
 
-const RandomDeath = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const RandomDeath = ({randomDeath, isLoading} ) => {
+    return isLoading ? <Loader /> :     
+    
+        <div className="randomDeath-container">
+                <div className="randomDeath-img-container">
+                    <img src={randomDeath.img} alt="" />
+                </div>
+                <div className="randomDeath-info">
+                    <h1>{randomDeath.death}</h1>
+                    <ul>
+                        <li>
+                            <strong>Cause of Death: </strong>
+                            {randomDeath.cause}
+                        </li>
+                        <li>
+                            <strong>Responsible: </strong>
+                            {randomDeath.responsible}
+                        </li>
+                        <li>
+                            <strong>Last Words: </strong>
+                            {randomDeath.last_words}
+                        </li>
+                    </ul>
+                </div>
+            </div>     
+    
+};
 
-export default RandomDeath
+export default RandomDeath;
